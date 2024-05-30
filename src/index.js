56a2c7099be86dd69a02caafe210073c96ace8e5
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-30 19:14:35 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImage } from './js/api';
 
-console.log('lol');
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const searchBtn = document.querySelector('[type=submit]');
+const loadMoreBtn = document.querySelector('.load-more');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(ev) {
+  ev.preventDefault();
+  const inputValue = ev.currentTarget.searchQuery.value.trim();
+  console.log(inputValue);
+}
+
+fetchImage().then(data => {
+  const markup = data.hits;
+
+  return markup.map(
+    ({
+      id,
+      largeImageURL,
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      gallery.innerHTML = `<div class="photo-card">
+                            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                            <div class="info">
+                             <p class="info-item"><b>Likes</b> ${likes}</p>
+                             <p class="info-item"><b>Views</b> ${views}</p>                
+                             <p class="info-item"><b>Comments</b> ${comments}</p>
+                             <p class="info-item"><b>Downloads</b> ${downloads}</p>
+                            </div> 
+                        </div>`;
+    }
+  );
+});
---
+++ src/js/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '44148621-e60a5c983575e679a6a81416c';
+
+export async function fetchImage() {
+  const resolve = await fetch(
+    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`
+  );
+  return await resolve.json();
+}
