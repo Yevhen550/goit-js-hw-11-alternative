ab526853355700e4ea6bea4d1ac50c0256136a34
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-06-06 18:01:25 +0300

    Update render-gallery.js

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -1,58 +1,58 @@
 const gallery = document.querySelector('.gallery');
 
-export function renderGallery(images) {
-  return images.map(
-    ({
-      id,
-      largeImageURL,
-      webformatURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      gallery.innerHTML = `<div class="photo-card">
-                            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                            <div class="info">
-                             <p class="info-item"><b>Likes</b> ${likes}</p>
-                             <p class="info-item"><b>Views</b> ${views}</p>
-                             <p class="info-item"><b>Comments</b> ${comments}</p>
-                             <p class="info-item"><b>Downloads</b> ${downloads}</p>
-                            </div>
-                        </div>`;
-    }
-  );
-}
-
 // export function renderGallery(images) {
-//   const markup = images
-//     .map(image => {
-//       const {
-//         id,
-//         largeImageURL,
-//         webformatURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       } = image;
-//       return `
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <div class="gallery-item" id="${id}">
-//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             <div class="info">
-//               <p class="info-item"><b>Likes</b>${likes}</p>
-//               <p class="info-item"><b>Views</b>${views}</p>
-//               <p class="info-item"><b>Comments</b>${comments}</p>
-//               <p class="info-item"><b>Downloads</b>${downloads}</p>
-//             </div>
-//           </div>
-//         </a>
-//       `;
-//     })
-//     .join('');
-
-//   gallery.insertAdjacentHTML('beforeend', markup);
+//   return images.map(
+//     ({
+//       id,
+//       largeImageURL,
+//       webformatURL,
+//       tags,
+//       likes,
+//       views,
+//       comments,
+//       downloads,
+//     }) => {
+//       gallery.innerHTML = `<div class="photo-card">
+//                             <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//                             <div class="info">
+//                              <p class="info-item"><b>Likes</b> ${likes}</p>
+//                              <p class="info-item"><b>Views</b> ${views}</p>
+//                              <p class="info-item"><b>Comments</b> ${comments}</p>
+//                              <p class="info-item"><b>Downloads</b> ${downloads}</p>
+//                             </div>
+//                         </div>`;
+//     }
+//   );
 // }
+
+export function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" width=150px hight=130px src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
