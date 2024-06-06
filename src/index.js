db641fbe668c333d9741dcd89b99f3b43d53c85a
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-06-06 17:08:57 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImage } from './js/api';
+import { renderGallery } from './js/render-gallery';
 
 const form = document.querySelector('.search-form');
-const gallery = document.querySelector('.gallery');
+
 const loadMoreBtn = document.querySelector('.load-more');
 
 form.addEventListener('submit', onSubmit);
@@ -17,29 +18,8 @@ function onSubmit(ev) {
 
 fetchImage().then(data => {
   const markup = data.hits;
-
-  return markup.map(
-    ({
-      id,
-      largeImageURL,
-      webformatURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      gallery.innerHTML = `<div class="photo-card">
-                            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                            <div class="info">
-                             <p class="info-item"><b>Likes</b> ${likes}</p>
-                             <p class="info-item"><b>Views</b> ${views}</p>                
-                             <p class="info-item"><b>Comments</b> ${comments}</p>
-                             <p class="info-item"><b>Downloads</b> ${downloads}</p>
-                            </div> 
-                        </div>`;
-    }
-  );
+  console.log(markup);
+  return renderGallery(markup);
 });
 
 function onLoadMoreBtn(ev) {
--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ const API_KEY = '44148621-e60a5c983575e679a6a81416c';
 
 export async function fetchImage() {
   const resolve = await fetch(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`
   );
   return await resolve.json();
 }
