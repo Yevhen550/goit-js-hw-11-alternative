5e41beae687d3ee7c8e48b4e2ea7db3bb73019c7
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-06-10 14:51:47 +0300

    Update render-gallery.js

--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -1,34 +1,5 @@
 const gallery = document.querySelector('.gallery');
 
-// export function renderGallery(images) {
-//   return images.map(
-//     ({
-//       id,
-//       largeImageURL,
-//       webformatURL,
-//       tags,
-//       likes,
-//       views,
-//       comments,
-//       downloads,
-//     }) => {
-//       gallery.innerHTML = `
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <div class="photo-card" id="${id}">
-//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             <div class="info">
-//               <p class="info-item"><b>Likes</b>${likes}</p>
-//               <p class="info-item"><b>Views</b>${views}</p>
-//               <p class="info-item"><b>Comments</b>${comments}</p>
-//               <p class="info-item"><b>Downloads</b>${downloads}</p>
-//             </div>
-//           </div>
-//         </a>
-//       `;
-//     }
-//   );
-// }
-
 export function renderGallery(images) {
   const markup = images
     .map(image => {
@@ -60,32 +31,3 @@ export function renderGallery(images) {
 
   gallery.insertAdjacentHTML('beforeend', markup);
 }
-
-// export function renderGallery(images) {
-//   return images.forEach(
-//     ({
-//       id,
-//       largeImageURL,
-//       webformatURL,
-//       tags,
-//       likes,
-//       views,
-//       comments,
-//       downloads,
-//     }) => {
-//       gallery.innerHTML = `
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <div class="photo-card" id="${id}">
-//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             <div class="info">
-//               <p class="info-item"><b>Likes</b>${likes}</p>
-//               <p class="info-item"><b>Views</b>${views}</p>
-//               <p class="info-item"><b>Comments</b>${comments}</p>
-//               <p class="info-item"><b>Downloads</b>${downloads}</p>
-//             </div>
-//           </div>
-//         </a>
-//       `;
-//     }
-//   );
-// }
